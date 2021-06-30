fd8c0de518200e63e04efe19e5f9543dd52170da
Ashish Ranjan Kumar <imashish2589@gmail.com>
2021-06-30 05:46:33 +0530

    Test commit

--- src/Body.jsx
+++ src/Body.jsx
@@ -6,15 +6,54 @@ let obj = {
     padding: '5px'
 }
 
+function func(containerid) {
+    if(containerid === '#output') {
+        var elm = document.querySelector(containerid);
+        var selection = window.getSelection();
+        var range = document.createRange();
+        range.selectNodeContents(elm);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        document.execCommand("Copy"); 
+    }
+    else {
+        var txtArea = document.querySelector(containerid);
+        txtArea.select();
+        document.execCommand("Copy"); 
+    }  
+}
+
+function download() {
+    let text = document.querySelector('#foutput').innerHTML ;
+    let fileName = 'output.txt';
+    var element = document.createElement('a');
+    element.setAttribute('href','data:text/plain;charset=utf-8,'+encodeURIComponent(text));
+    element.setAttribute('download',fileName);
+    element.setAttribute('target','_blank');
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+
 function Body() {
     return (
         <>
             <div className="body">
                 <div id="editor"></div>
                 <div className="inp-out">
-                    <p> Input </p>
-                    <textarea id="input" style={obj} placeholder="Enter input here..."></textarea>
-                    <p> Output </p>
+                    <div id="inp"> 
+                        <span> Input </span>
+                        <i onClick={() => { func('#input') }} style={{position: 'absolute',right:'0.5rem'}} className="fas fa-copy crs"></i>
+                        <i style={{position: 'absolute',right:'2.2rem'}} className="fa fa-folder-open crs" aria-hidden="true"></i>
+                        <input type="file" style={{display: 'none'}}/>
+                    </div>
+                    <textarea type="text" id="input" style={obj} placeholder="Enter input here..."></textarea>
+                    <div id="out"> 
+                        <span>Output</span>
+                        <i onClick={() => { func('#output') }} style={{position: 'absolute',right:'0.5rem'}} className="fas fa-copy crs"></i>
+                        <i onClick={download} style={{position: 'absolute',right:'2.2rem'}} className="fa fa-download crs" aria-hidden="true"></i>
+                    </div>
                     <div id="output" style={obj}>
                         <pre id="foutput"> </pre>
                     </div>
